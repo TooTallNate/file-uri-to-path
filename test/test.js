e171f393ba386a03546bd740762be2f8a0ff136d
John-David Dalton <john.david.dalton@gmail.com>
2017-05-18 12:37:04 -0700

    Update test syntax.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,22 @@
+'use strict';
 
-var sep = require('path').sep || '/';
-var assert = require('assert');
-var uri2path = require('../');
-var tests = require('./tests.json');
+const assert = require('assert');
+const path = require('path');
+const tests = require('./tests.json');
+const uri2path = require('../');
 
-describe('file-uri-to-path', function () {
+describe('file-uri-to-path', () => {
+  const toString = JSON.stringify;
 
-  Object.keys(tests).forEach(function (uri) {
-
+  Object.keys(tests).forEach((uri) => {
     // the test cases were generated from Windows' PathCreateFromUrlA() function.
     // On Unix, we have to replace the path separator with the Unix one instead of
     // the Windows one.
-    var expected = tests[uri].replace(/\\/g, sep);
+    const expected = tests[uri].replace(/\\/g, path.sep);
 
-    it('should convert ' + JSON.stringify(uri) + ' to ' + JSON.stringify(expected),
-    function () {
-      var actual = uri2path(uri);
-      assert.equal(actual, expected);
+    it(`should convert ${toString(uri)} to ${toString(expected)}`, () => {
+      assert.equal(uri2path(uri), expected);
     });
-
   });
 
 });
