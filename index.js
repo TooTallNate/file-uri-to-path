69bcb9e1705b13b760eb64bb4c30745464353ff7
John-David Dalton <john.david.dalton@gmail.com>
2017-05-18 12:36:34 -0700

    Use built-in url module.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
 
 /**
  * Module dependencies.
  */
 
-var sep = require('path').sep || '/';
+const path = require('path');
+const url = require('url');
 
 /**
  * Module exports.
@@ -20,47 +22,40 @@ module.exports = fileUriToPath;
  */
 
 function fileUriToPath (uri) {
-  if ('string' != typeof uri ||
-      uri.length <= 7 ||
-      'file://' != uri.substring(0, 7)) {
+  uri = url.parse(uri);
+
+  if (uri.protocol !== 'file:' || url.pathname === null) {
     throw new TypeError('must pass in a file:// URI to convert to a file path');
   }
-
-  var rest = unescape(uri.substring(7));
-  var firstSlash = rest.indexOf('/');
-  var host = rest.substring(0, firstSlash);
-  var path = rest.substring(firstSlash + 1);
+  // Based on the file URI Scheme:
+  // https://tools.ietf.org/html/draft-hoffman-file-uri-03
+  let host = uri.host;
+  let pathname = unescape(uri.pathname);
 
   // 2.  Scheme Definition
   // As a special case, <host> can be the string "localhost" or the empty
   // string; this is interpreted as "the machine from which the URL is
   // being interpreted".
-  if ('localhost' == host) host = '';
-
-  if (host) {
-    host = sep + sep + host;
+  if (host === 'localhost') {
+    host = '';
+  }
+  else if (host) {
+    host = path.sep + path.sep + host;
+  }
+  else {
+    // Windows shares will have a pathname starting with "//".
+    host = pathname.match(/^\/\/[^/]+|/)[0];
+    pathname = pathname.slice(host.length);
   }
 
   // 3.2  Drives, drive letters, mount points, file system root
   // Drive letters are mapped into the top of a file URI in various ways,
   // depending on the implementation; some applications substitute
   // vertical bar ("|") for the colon after the drive letter, yielding
-  // "file:///c|/tmp/test.txt".  In some cases, the colon is left
-  // unchanged, as in "file:///c:/tmp/test.txt".  In other cases, the
+  // "file:///c|/tmp/test.txt". In some cases, the colon is left
+  // unchanged, as in "file:///c:/tmp/test.txt". In other cases, the
   // colon is simply omitted, as in "file:///c/tmp/test.txt".
-  path = path.replace(/^(.+)\|/, '$1:');
+  pathname = path.normalize(pathname.replace(/^\/([a-zA-Z])[:|]/, '$1:'));
 
-  // for Windows, we need to invert the path separators from what a URI uses
-  if (sep == '\\') {
-    path = path.replace(/\//g, '\\');
-  }
-
-  if (/^.+\:/.test(path)) {
-    // has Windows drive at beginning of path
-  } else {
-    // unix path…
-    path = sep + path;
-  }
-
-  return host + path;
+  return host + pathname;
 }
